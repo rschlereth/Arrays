1f3f56e1b9189d3117e0f6ab54d6217fc2d02513
Robert Schlereth <robert.schlereth@gmail.com>
2022-12-07 10:44:49 -0600

    Apply methods to variables

--- arrays.js
+++ arrays.js
@@ -1,3 +1,16 @@
-var cereals = ("Zeroes", "Squares", "Honey Bunches", "Crispies")
-var calories = (50, 75, 120, 65)
-var healthful = (true, true, false, true)
+var cereals = ["Zeroes", "Squares", "Honey Bunches", "Crispies"]
+var calories = [50, 75, 120, 65]
+var healthful = [true, true, false, true]
+
+//should return "Zeroes", "Squares", "Honey Bunches"
+cereals.pop();
+console.log(cereals);
+
+//should log [50, 75. 120, 65, 150]
+calories.push(150);
+console.log(calories);
+
+//should now append at the begining "false", so result will be "false, 
+//true, true, false, true"
+healthful.unshift(false);
+console.log(healthful);
