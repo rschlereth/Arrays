0c573fb2b8d4eb16976c14ebd1149aad1efb6519
Robert Schlereth <robert.schlereth@gmail.com>
2022-12-07 10:59:55 -0600

    Add methods to established arrays

--- arrays.js
+++ arrays.js
@@ -11,6 +11,17 @@ calories.push(150);
 console.log(calories);
 
 //should now append at the begining "false", so result will be "false, 
-//true, true, false, true"
+//true, true, false, true".
 healthful.unshift(false);
 console.log(healthful);
+
+//calling index position 2, which should result in Honey Bunches, 
+//which to our eyes is the third element.
+console.log(cereals[2])
+
+//should log "65"
+console.log(calories[3])
+
+//should return the value of the first element; note that because I 
+//unshifted healthful above, the first element is actually "false".
+console.log(healthful[0]);
